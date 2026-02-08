5fc1badbd0b235b5055d17c75af867ee1517e370
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-08 09:56:25 +0800

    tailwind and postcss config file type to ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,3 +1,5 @@
+import type { Config } from "tailwindcss";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -19,4 +21,4 @@ export default {
 		},
 	},
 	plugins: [],
-};
+} satisfies Config;
