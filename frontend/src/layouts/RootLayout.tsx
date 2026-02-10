eccbce2cd8b0cf86f90a4c7a9139d5426ae10e5e
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-10 16:45:04 +0800

    root layout

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,17 +1,23 @@
-// routes/layouts/RootLayout.jsx
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import PageLoader from "../components/PageLoader";
 
 export default function RootLayout() {
+	const [appReady, setAppReady] = useState(false);
+
+	if (!appReady) {
+		return <PageLoader onFinish={() => setAppReady(true)} />;
+	}
+
 	return (
-		<div className="bg-black h-screen">
-			<Navbar />
+		<div className="animate-fadeIn">
 			<Suspense fallback={null}>
+				<Navbar />
 				<Outlet />
+				<Footer />
 			</Suspense>
-			<Footer />
 		</div>
 	);
 }
