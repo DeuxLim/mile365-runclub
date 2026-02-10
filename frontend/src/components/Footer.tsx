694cd2388deb9d34e54525ed29ad8ca6d07ab6bb
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-10 16:45:32 +0800

    footer placeholder

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-	return <div>footer</div>;
+	return (
+	<div className="h-60 border flex justify-center items-center font-black text-6xl">
+		Footer
+	</div>
+	);
 }
