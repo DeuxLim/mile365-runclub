75c1a4094e7ce3bc402f13828774281610cc3081
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-11 20:42:11 +0800

    initial about page

---
+++ frontend/src/features/landing/pages/About.tsx
@@ -0,0 +1,54 @@
+export default function About() {
+	return (
+		<section id="about" className="h-screen flex flex-col p-4">
+			<div className="h-[40%] flex justify-center items-center flex-col gap-4">
+				<h1 className="font-heading text-4xl md:text-7xl font-black">
+					Built on Consistency.
+					<br />
+					Driven by Community.
+				</h1>
+
+				<div className="flex flex-col px-5">
+					<div className="font-body text-gray-300 text-sm text-center md:text-lg">
+						MILE 365 Run Club is a Bulacan-based running community
+						built for beginners to performance runners who believe
+						in daily progress.
+					</div>
+
+					<div className="text-center text-gray-300 text-sm md:text-lg">
+						We train with structure. We run with discipline. We grow
+						together — 365 days a year.
+					</div>
+				</div>
+			</div>
+
+			<div className="flex flex-1 p-20">
+				<div className="w-[40%] flex justify-start items-center">
+					<ul
+						className="
+                        p-20 
+                        font-heading
+                        text-4xl 
+                        font-semibold
+                        w-[90%]
+                        flex
+                        flex-col
+                        gap-10
+                        [&>li]:border-b
+                        [&>li]:border-gray-400
+                        [&>li]:p-2
+                        "
+					>
+						<li>Consistency</li>
+						<li>Structure</li>
+						<li>Community</li>
+					</ul>
+				</div>
+
+				<div className="w-[60%] flex justify-center items-center">
+					<div className="w-full border h-full m-10"></div>
+				</div>
+			</div>
+		</section>
+	);
+}
