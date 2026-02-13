6bfe0b5c2b4dc33106c1e67f56fe48f73c1fcf39
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-13 21:26:24 +0800

    initiate target component

---
+++ frontend/src/features/landing/pages/Target.tsx
@@ -0,0 +1,10 @@
+export default function Target() {
+	return (
+		<section
+			id="runs"
+			className="h-screen border flex justify-center items-center text-9xl font-extrabold"
+		>
+			Target
+		</section>
+	);
+}
