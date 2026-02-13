fd933040c12060bb556ecda359bb837df770a504
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-13 21:26:34 +0800

    initiante schedule component

---
+++ frontend/src/features/landing/pages/Schedule.tsx
@@ -0,0 +1,10 @@
+export default function Schedule() {
+	return (
+		<section
+			id="runs"
+			className="h-screen border flex justify-center items-center text-9xl font-extrabold"
+		>
+			Schedule
+		</section>
+	);
+}
