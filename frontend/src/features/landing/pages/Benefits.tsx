d2ed0311566b952c45f277762674c61e7f0a0fcb
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-13 21:26:43 +0800

    initiate benefits component

---
+++ frontend/src/features/landing/pages/Benefits.tsx
@@ -0,0 +1,10 @@
+export default function Benefits() {
+	return (
+		<section
+			id="community"
+			className="h-screen border flex justify-center items-center text-9xl font-extrabold"
+		>
+			Benefits
+		</section>
+	);
+}
