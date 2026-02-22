a611b083e40625264bffa7badbf3f847c907d062
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-22 17:41:36 +0800

    register route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,4 @@ Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::patch('/membership-requests/{id}/reject', [MembershipController::class, 'reject']);
 });
 Route::post('/admin/login', [AuthController::class, 'login']);
+Route::post('/admin/register', [AuthController::class, 'register']);
