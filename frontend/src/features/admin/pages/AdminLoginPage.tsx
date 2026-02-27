7e59936548b1e56bdad820486d97eef340f0d98a
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-27 19:44:01 +0800

    login page layout

--- frontend/src/features/admin/pages/AdminLoginPage.tsx
+++ frontend/src/features/admin/pages/AdminLoginPage.tsx
@@ -1,9 +1,15 @@
 import AdminLoginForm from "@/features/admin/components/AdminLoginForm";
+import logo from "@/assets/photos/365_logo_dark.png";
 
 export default function AdminLoginPage() {
 	return (
-		<div className="">
-			The team needs your leadership and guidance.
+		<div className="min-h-screen bg-white text-black flex flex-col items-center justify-center">
+			<img
+				src={logo}
+				className="h-20 md:h-20 object-contain transition-all duration-300 mb-6"
+				alt="Mile 365 Logo"
+			/>
+			<h1 className="text-3xl">ADMINS 365 PORTAL</h1>
 			<AdminLoginForm />
 		</div>
 	);
