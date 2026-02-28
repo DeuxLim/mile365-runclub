f39e87537e505278cca2559114b8a08f14f8ce38
Deux Daniel Lim <limdeux27@gmail.com>
2026-02-28 14:39:22 +0800

    guard login route to only allow unauthenticated user

--- frontend/src/features/admin/pages/AdminLoginPage.tsx
+++ frontend/src/features/admin/pages/AdminLoginPage.tsx
@@ -1,7 +1,23 @@
 import AdminLoginForm from "@/features/admin/components/AdminLoginForm";
 import logo from "@/assets/photos/365_logo_dark.png";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import { useAdmin } from "@/hooks/useAuthenticatedAdmin";
 
 export default function AdminLoginPage() {
+	const navigate = useNavigate();
+	const { data: admin, isPending } = useAdmin();
+
+	useEffect(() => {
+		if (admin) {
+			navigate("/admin/dashboard", { replace: true });
+		}
+	}, [admin, navigate]);
+
+	if (isPending) {
+		return null;
+	}
+
 	return (
 		<div className="min-h-screen bg-white text-black flex flex-col items-center justify-center">
 			<img
