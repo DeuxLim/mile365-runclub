9476f06ce33cbf1ade3b00811ebe8044875bd50d
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-01 20:44:13 +0800

    addded logout route on api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,11 +11,11 @@ Route::get('/letsgo365', function () {
 });
 
 Route::post('/membership-requests', [MembershipController::class, 'store']);
+Route::post('/admin/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/membership-requests', [MembershipController::class, 'index']);
     Route::patch('/membership-requests/{id}/approve', [MembershipController::class, 'approve']);
     Route::patch('/membership-requests/{id}/reject', [MembershipController::class, 'reject']);
 });
-
-Route::post('/admin/login', [AuthController::class, 'login']);
